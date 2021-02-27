304164f3228de368959728631959314be99be49f
IlkeYilmaz <56206546+ilkeyil@users.noreply.github.com>
2021-02-27 12:36:02 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const enteredPlaintext = "";
+const outputtedCiphertext = "";
+const plaintext_span = document.getElementById("plaintext-label");
+const ciphertext_span = document.getElementById("ciphertext-label");
+
