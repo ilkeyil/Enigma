49efda88956e73af4d58ca9e7fd574b86c1c088f
IlkeYilmaz <56206546+ilkeyil@users.noreply.github.com>
2021-03-11 12:07:40 +0100

    Basic JavaScript functions are declared. The content of the functions are being written. The functions can be called and working and they are being developed.

--- app.js
+++ app.js
@@ -2,4 +2,134 @@ const enteredPlaintext = "";
 const outputtedCiphertext = "";
 const plaintext_span = document.getElementById("plaintext-label");
 const ciphertext_span = document.getElementById("ciphertext-label");
+const board_div = document.querySelector(".board");
+const a_div = document.getElementById("a");
+const b_div = document.getElementById("b");
+const c_div = document.getElementById("c");
+const d_div = document.getElementById("d");
+const e_div = document.getElementById("e");
+const f_div = document.getElementById("f");
+const g_div = document.getElementById("g");
+const h_div = document.getElementById("h");
+const i_div = document.getElementById("i");
+const j_div = document.getElementById("j");
+const k_div = document.getElementById("k");
+const l_div = document.getElementById("l");
+const m_div = document.getElementById("m");
+const n_div = document.getElementById("n");
+const o_div = document.getElementById("o");
+const p_div = document.getElementById("p");
+const q_div = document.getElementById("q");
+const r_div = document.getElementById("r");
+const s_div = document.getElementById("s");
+const t_div = document.getElementById("t");
+const u_div = document.getElementById("u");
+const v_div = document.getElementById("v");
+const w_div = document.getElementById("w");
+const x_div = document.getElementById("x");
+const y_div = document.getElementById("y");
+const z_div = document.getElementById("z");
+
+function userInput(userChoice){
+    console.log(userChoice);
+}
+
+function computerEncrypt(){
+
+}
+
+function main()
+{
+    a_div.addEventListener('click', function () {
+        userInput("a");
+    })
+
+    b_div.addEventListener('click', function () {
+        userInput("b");
+    })
+
+    c_div.addEventListener('click', function () {
+        userInput("c");
+    })
+
+    d_div.addEventListener('click', function () {
+        userInput("d");
+    })
+
+    e_div.addEventListener('click', function () {
+        userInput("e");
+    })
+
+    f_div.addEventListener('click', function () {
+        userInput("f");    })
+
+    g_div.addEventListener('click', function () {
+        userInput("g");    })
+
+    h_div.addEventListener('click', function () {
+        userInput("h");    })
+
+    i_div.addEventListener('click', function () {
+        userInput("i");    })
+
+    j_div.addEventListener('click', function () {
+        userInput("j");    })
+
+    k_div.addEventListener('click', function () {
+        userInput("k");    })
+
+    l_div.addEventListener('click', function () {
+        userInput("l");    })
+
+    m_div.addEventListener('click', function () {
+        userInput("m");    })
+
+    n_div.addEventListener('click', function () {
+        userInput("n");    })
+
+    o_div.addEventListener('click', function () {
+        userInput("o");    })
+
+    p_div.addEventListener('click', function () {
+        userInput("p");    })
+
+    q_div.addEventListener('click', function () {
+        userInput("g");    })
+
+    r_div.addEventListener('click', function () {
+        userInput("r");    })
+
+    s_div.addEventListener('click', function () {
+        userInput("s");    })
+
+    t_div.addEventListener('click', function () {
+        userInput("t");    })
+
+    u_div.addEventListener('click', function () {
+        userInput("u");    })
+
+    v_div.addEventListener('click', function () {
+        userInput("v");    })
+
+    w_div.addEventListener('click', function () {
+        userInput("w");    })
+
+    x_div.addEventListener('click', function () {
+        userInput("x");    })
+
+    y_div.addEventListener('click', function () {
+        userInput("y");    })
+
+    z_div.addEventListener('click', function () {
+        userInput("z");    })
+} // main() ends here
+
+main();
+
+
+
+
+
+
+
 
